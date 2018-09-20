806269713aa9df50a9960c55e4d48a709154a3fa
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-09-20 18:53:02 +0900

    Update graph

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,21 +5,17 @@ class HomeController < ApplicationController
      @plans = Plan.all
 
     users_tmp = @userplans.pluck(:user_id)
-    satisfied = @userplans.pluck(:satisfied)
+    satisfied = @userplans.pluck(:satisfied).sort.reverse
 
     @chart = LazyHighCharts::HighChart.new("graph") do |c|
       c.title(text: "satisfied graph")
-      # X軸の名称を設定 '月'
       c.xAxis(categories: users_tmp, title: {text: '人'})
-      # Y軸の名称を設定 '円'
-      c.yAxis(categories: satisfied, title: {text: '満足度'})
+      c.yAxis(title: {text: '満足度'},
+       ceiling: 100)
       c.series(name: "A", data: satisfied)
-      # 判例を右側に配置
-      @debug = "Ok"
-     c.legend(align: 'right', verticalAlign: 'top',
+      c.legend(align: 'right', verticalAlign: 'top',
        x: 0, y: 100, layout: 'vertical')
-     # グラフの種類を「折れ線グラフ」から「棒グラフ」に変更
-     c.chart(type: "column")
+      c.chart(type: "column")
    end
   end
 end
