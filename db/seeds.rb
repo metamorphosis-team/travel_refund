82d557508c564df5fb16c5888c86d3e89cd88b8b
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-09-20 16:04:21 +0900

    Add data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+csv_data = CSV.read('db/users.csv', headers: true)
+csv_data.each do |data|
+  User.create!(data.to_hash)
+end
+csv_data1 = CSV.read('db/plan.csv', headers: true)
+csv_data1.each do |data|
+  Plan.create!(data.to_hash)
+end
+csv_data2 = CSV.read('db/user-plan.csv', headers: true)
+csv_data2.each do |data|
+  Userplan.create!(data.to_hash)
+end
