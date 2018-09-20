8dece49826d85c040030e097772edc2a206fef91
Ryuto Kawano <q111011r@mail.kyutech.jp>
2018-09-20 17:15:10 +0900

    Add graph

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,25 @@
 class HomeController < ApplicationController
   def top
-    @userplans = Userplan.all
-    @users = User.all
-    @plans = Plan.all
+     @userplans = Userplan.all
+     @users = User.all
+     @plans = Plan.all
+
+    users_tmp = @userplans.pluck(:user_id)
+    satisfied = @userplans.pluck(:satisfied)
+
+    @chart = LazyHighCharts::HighChart.new("graph") do |c|
+      c.title(text: "satisfied graph")
+      # X軸の名称を設定 '月'
+      c.xAxis(categories: users_tmp, title: {text: '人'})
+      # Y軸の名称を設定 '円'
+      c.yAxis(title: {text: '満足度'})
+      c.series(name: "A", data: satisfied)
+      # 判例を右側に配置
+      @debug = "Ok"
+     c.legend(align: 'right', verticalAlign: 'top',
+       x: 0, y: 100, layout: 'vertical')
+     # グラフの種類を「折れ線グラフ」から「棒グラフ」に変更
+     c.chart(type: "column")
+   end
   end
 end
